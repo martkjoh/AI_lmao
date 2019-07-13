97786d78e8ce0d222c401e7af3d56be1fb049765
Martin <martin.johnsrud@gmail.com>
2019-07-14 00:12:18 +0200

    added gitignore (a bit late)

--- main.cpp
+++ main.cpp
@@ -3,12 +3,11 @@
 int main()
 {
     float a[] =  {
-        2, 
-        2, 
-        2
+        {2,}, 
+        {2,}, 
+        {2,}
     };
-    cout << a[0] << endl;
-    // Matrix L{2, 1, a};
+    Matrix L{3, 1, a};
 
     return 0;
 }

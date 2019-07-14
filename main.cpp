e90430bd4ea9d1a60fac376b7ddf585597af32fd
Martin <martin.johnsrud@gmail.com>
2019-07-14 19:17:04 +0200

    Written som theory

--- main.cpp
+++ main.cpp
@@ -3,25 +3,6 @@
 int main()
 {
     // srand (time(NULL));
-    float d[] = {
-        3,
-        4,
-        2
-    };
 
-    float d2[] = {
-        1,
-        0
-    };
-
-    Matrix L{3, 1, d};
-
-    NeuralNet N(L);  
-
-    Matrix in = rand(3, 1);
-    Matrix y(2, 1, d2);
-    N.activate(in);
-    N.printNet();
-    cout << C(N.getOutput(), y);
     return 0;
 }
